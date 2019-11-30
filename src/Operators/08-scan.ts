6eb559be1d5e3a2c093ae89b6d1b2a047ffbd658
Anibal <anibal.alvarez@readinessit.com>
2019-11-30 16:37:39 -0300

    scan

---
+++ src/Operators/08-scan.ts
@@ -0,0 +1,46 @@
+import { from } from "rxjs";
+import { reduce, scan, map } from "rxjs/operators";
+
+const numbers = [1,2,3,4,5];
+
+const totalAcumulador = (acc: number, cur: number) => acc + cur;
+
+//Reduce
+from( numbers ).pipe(
+    reduce( totalAcumulador, 0)
+)
+.subscribe( console.log );
+
+//scan
+from( numbers ).pipe(
+    scan( totalAcumulador, 0)
+)
+.subscribe( console.log );
+
+
+// Redux
+interface Usuario {
+    id?: string;
+    autenticado?: boolean;
+    token?: string;
+    edad?: number;
+}
+const user: Usuario[] =[
+    {id:'fher', autenticado: false, token: null},
+    {id:'fher', autenticado: true, token: 'ABC'},
+    {id:'fher', autenticado: true, token: 'ABC123'}
+];
+
+const state$ = from( user ).pipe(
+    scan<Usuario>((acc, cur) => {
+        console.log('1-',acc);
+        console.log('2-',cur);
+        return { ...acc, ...cur}
+    }, {edad: 33})
+);
+
+const id$ = state$.pipe(
+    map( state => state )
+);
+
+id$.subscribe( console.log );
