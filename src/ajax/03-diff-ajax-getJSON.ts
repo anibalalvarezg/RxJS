8feae1450a91d472407690a5eca5a82e37dc57f5
Anibal Alvarez <anibal.alvarezg@gmail.com>
2019-12-09 23:01:03 -0300

    diff ajax getJSON

---
+++ src/ajax/03-diff-ajax-getJSON.ts
@@ -0,0 +1,27 @@
+import { ajax, AjaxError } from 'rxjs/ajax';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+const url = 'https://httpbinss.org/delay/1';
+
+const errorManag = ( resp: AjaxError) => {
+    console.warn( 'error: ', resp.message );
+    return of({
+        ok: false,
+        usuarios: []
+    })
+}
+// const obs$ = ajax.getJSON( url ).pipe(catchError(errorManag));
+// const obs2$ = ajax( url );
+
+const obs$ = ajax.getJSON( url ).pipe(
+    catchError(errorManag)
+);
+
+obs$.subscribe({
+    next: (val) => console.log('next: ', val),
+    error: (error) => console.warn('error: ', error),
+    complete: () => console.log( 'complete' ),
+});
+
