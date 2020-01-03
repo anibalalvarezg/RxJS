10fabc392a079aec156b8d2dad8599c6e3111ff9
Anibal Alvarez <anibal.alvarezg@gmail.com>
2020-01-02 21:39:10 -0300

    exhaustMap

--- src/Operadores-transformacion/06-exhaustMap.ts
+++ src/Operadores-transformacion/06-exhaustMap.ts
@@ -1,5 +1,5 @@
 import { interval, fromEvent } from 'rxjs';
-import { take, switchMap, concatMap, exhaustMap } from 'rxjs/operators';
+import { take, exhaustMap } from 'rxjs/operators';
 
 const interval$= interval(500).pipe(take(3));
 const click$ = fromEvent( document, 'click');
